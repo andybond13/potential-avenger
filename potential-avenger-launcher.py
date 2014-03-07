0b694a419162c476973a15afb9b75e7354ac72a7
Andy @ Office <andybond13@yahoo.com>
2014-03-07 15:05:02 -0500

    Add oneAtATime specification to luncher script.

--- potential-avenger-launcher.py
+++ potential-avenger-launcher.py
@@ -6,15 +6,16 @@ command = "rm *.vtk"
 os.system(command)
 
 #define input arguments
-ts_refine = 2
-end_t = 2
-Nelt = 20
-lc = 0.02
-intOrder = 2
-strain_rate = 2.5
-printVTK = 0
+ts_refine = 4
+end_t = 0.7
+Nelt = 100
+lc = 0.09
+intOrder = 1
+strain_rate = 1.0
+printVTK = 1
+oneAtATime = 1
 
 #run program
-command = "./potential-avenger.exe %f %f %f %u %f %u %u" % (strain_rate, ts_refine, end_t, Nelt, lc, intOrder, printVTK)
+command = "./potential-avenger.exe %f %f %f %u %f %u %u %u" % (strain_rate, ts_refine, end_t, Nelt, lc, intOrder, printVTK, oneAtATime)
 print command
 os.system(command)
