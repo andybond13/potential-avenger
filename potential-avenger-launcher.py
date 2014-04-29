01784b580560c765ecf563346b638fb48e34b6ba
Andrew Stershic <andybond13@yahoo.com>
2014-04-29 18:51:15 -0400

    Add input for alpha and local only to launcher

--- potential-avenger-launcher.py
+++ potential-avenger-launcher.py
@@ -15,8 +15,10 @@ strain_rate = 1.0
 printVTK = 1
 oneAtATime = 1
 minOpenDist = 0.05
+alpha = 0.5		#hardening coefficient bounded by 0 & 1: 0-very brittle, 1-very plastic
+localOnly = 1	#1 for local only, 0 for local/non-local
 
 #run program
-command = "./potential-avenger.exe %f %f %f %u %f %u %u %u %f" % (strain_rate, ts_refine, end_t, Nelt, lc, intOrder, printVTK, oneAtATime, minOpenDist)
+command = "./potential-avenger.exe %f %f %f %u %f %u %u %u %f %f %u" % (strain_rate, ts_refine, end_t, Nelt, lc, intOrder, printVTK, oneAtATime, minOpenDist, alpha, localOnly)
 print command
 os.system(command)
